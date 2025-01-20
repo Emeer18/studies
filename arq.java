293dea2342ad7267e08b8aabe5d8ae3e0ad2da6c
Emerson Lucas Bertelli Cardoso <emersonn.lbc@gmail.com>
2025-01-20 20:21:32 +0000

    Data entry

--- arq.java
+++ arq.java
@@ -1,11 +1,14 @@
+import java.util.close();
 public class arq {
     public static void main(String[] args){
 
+    Scanner sc = new Scanner(System.in);
     System.out.println("Teste");
-    double b1 = 2.645484684684;
+    double b1;
+    sc.nextDouble(b1);
     System.out.printf("teste %.2f", b1);
 
     
-    
+    sc.close();
     }
 }
