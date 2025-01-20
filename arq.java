5188488c9e4e7224ba12e38ee299a20a5e106fea
Emerson Lucas Bertelli Cardoso <emersonn.lbc@gmail.com>
2025-01-20 16:28:48 +0000

    Variables, Concatenation, Syntax

--- arq.java
+++ arq.java
@@ -1,7 +1,11 @@
 public class arq {
-public static void main(String[] args){
+    public static void main(String[] args){
 
     System.out.println("Teste");
+    double b1 = 2.645484684684;
+    System.out.printf("teste %.2f", b1);
 
-}
+    
+    
+    }
 }
