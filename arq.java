873ccee87f72b68a0b8d00139428dd0fdde0fedc
Emerson Lucas Bertelli Cardoso <emersonn.lbc@gmail.com>
2025-01-23 20:14:15 +0000

    Repeating structures

--- arq.java
+++ arq.java
@@ -5,8 +5,9 @@ public class arq {
 
     Scanner sc = new Scanner(System.in);
     
-    int escolha;
-
+    int senha = 0;
+    int tentativas = 0;
+    boolean validaSenha = false;
     //System.out.println("Digite o dia da semana sendo a para segunda ....");  
     //escolha = sc.nextInt();
 
@@ -14,19 +15,58 @@ public class arq {
 
     //System.out.printf("%d", ternario);
     
-    //sc.close();
-    System.out.println("Digite um número : ");
-    escolha = sc.nextInt();
-    System.out.println("Digite a idade : ");
-    int idade = sc.nextInt();
+    
+    //System.out.println("Digite um número : ");
+    //escolha = sc.nextInt();
+    //System.out.println("Digite a idade : ");
+    //int idade = sc.nextInt();
 
-    String parImpar = (escolha %2 == 0) ? "par" : "impar";
+    //String parImpar = (escolha %2 == 0) ? "par" : "impar";
 
-    System.out.printf("O número é %s\n", parImpar);
+    //System.out.printf("O número é %s\n", parImpar);
 
-    String maiorIdade = (idade >= 18) ? "maior de idade" : "menor de idade";
+    //String maiorIdade = (idade >= 18) ? "maior de idade" : "menor de idade";
 
-    System.out.printf("Esta pessoa é %s", maiorIdade);
+    //System.out.printf("Esta pessoa é %s", maiorIdade);
+    
+    //while (escolha > 12) {
+
+     //   System.out.println("Teste");
+       // escolha = sc.nextInt();
+        
+    //}
+    for (tentativas = 2 ; senha != 2332 ; tentativas -- ){
+        
+        System.out.println("Digite a senha\n");
+        senha = sc.nextInt();
+        System.out.printf("Senha incorreta, tentativas restantes : %d\n", tentativas);
+        
+        if (senha == 2332) {
+
+            validaSenha = true;
+            
+        }
+
+        if (tentativas == 0) {
+
+            System.out.println("Acesso negado. Senha bloqueada\n");
+            break;
+                
+        }
+    }
+
+    if (validaSenha == true) {
+
+        System.out.println("Entrada validada.");
+        
+    }
+
+    System.out.println("Encerrado.");
+
+    
+    sc.close();
+
+  
 
     }
 
