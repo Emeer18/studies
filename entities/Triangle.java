95470351b5d452059762a37d58f73e37321ea19c
Emerson Lucas Bertelli Cardoso <emersonn.lbc@gmail.com>
2025-01-30 11:49:13 +0000

    Problem solved with Methods.

--- entities/Triangle.java
+++ entities/Triangle.java
@@ -6,6 +6,13 @@ public class Triangle {
     public Double c;
     public Double p;
 
+    public double area() {
+
+        p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        
+    }
+
     public static void imprimeArea(double xa, double ya){
 
         if (xa > ya) {
--- entities/arq.java
+++ entities/arq.java
@@ -11,19 +11,19 @@ public class arq {
     y = new Triangle();
 
     System.out.println("Enter the measures of area X");
+
     x.a = sc.nextDouble();
     x.b = sc.nextDouble();
     x.c = sc.nextDouble();
+
     System.out.println("Enter the measures of area Y");
+    
     y.a = sc.nextDouble();
     y.b = sc.nextDouble();
     y.c = sc.nextDouble();
 
-    x.p = (x.a + x.b + x.c) / 2.0;
-    double XArea = Math.sqrt(x.p * (x.p - x.a) * (x.p - x.b) * (x.p - x.c));
-
-    y.p = (y.a + y.b + y.c) / 2.0;
-    double YArea= Math.sqrt(y.p * (y.p - y.a) * (y.p - y.b) * (y.p - y.c));
+    double XArea = x.area();
+    double YArea= y.area();
     
     Triangle.imprimeArea(XArea, YArea);
 
