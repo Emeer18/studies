fde7f2de2ebd173f5af769ceb1eda49a68aea24a
Emerson Lucas Bertelli Cardoso <emersonn.lbc@gmail.com>
2025-02-11 23:49:09 +0000

    Working with arrays

--- entities/File.java
+++ entities/File.java
@@ -5,59 +5,25 @@ public class File {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        Bank bank = null;
-        int choice = 0;
         
-        System.out.println("Enter your account number and account holder's name.");
-        int account = sc.nextInt();
-        sc.nextLine();
-        String name = sc.nextLine();
-        
-        do {
-            System.out.println("Enter 1 to deposit an initial amount or 2 to start without a deposit.");
-            choice = sc.nextInt();
-            sc.nextLine();
+        int n = sc.nextInt();
+        double sum = 0;
+
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++){
             
-            if (choice == 1) {
-                System.out.println("Enter the initial deposit amount.");
-                double deposit = sc.nextDouble();
-                bank = new Bank(name, account, deposit);
-                bank.showData();
-            } 
-            else if (choice == 2) {
-                bank = new Bank(name, account);
-                bank.showData();
-            } 
-            else {
-                System.out.println("Please enter a valid option!");
-            }
-        } while (choice != 1 && choice != 2);
+            System.out.println("Digita aí");
+            vect[i] = sc.nextDouble();
+        }
 
-        System.out.println("Congratulations, your account has been created!");
-        bank.resetChoice(choice);
+        for(int i = 0; i < n; i++){
+            sum += vect [i];
+        }
 
-        do {
-            System.out.println("1 - Deposit.\n2 - Withdraw.\n3 - Change name.\n4 - Exit.");
-            choice = sc.nextInt();
-            sc.nextLine();
+        double avg = sum / n;
+        System.out.printf("AVERAGE = %.2f", avg);
 
-            if (choice == 1) {
-                System.out.println("Enter the amount to be deposited.");
-                double deposit = sc.nextDouble();
-                bank.deposit(deposit);
-                bank.showData();
-            } 
-            else if (choice == 2) {
-                System.out.println("Enter the amount to be withdrawn.");
-                double withdrawal = sc.nextDouble();
-                bank.withdraw(withdrawal);
-                bank.showData();
-            } 
-            else if (choice != 1 && choice != 2) {
-                System.out.println("Please enter a valid option!");
-            }
-        } while (choice != 4);
-
-        System.out.println("Test completed.");
+        sc.close();
     }
 }
