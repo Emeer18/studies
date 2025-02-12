f0a3b6a65f7f895dc666d89bc2968083f6d00748
Emerson Lucas Bertelli Cardoso <emersonn.lbc@gmail.com>
2025-02-12 18:42:14 +0000

    Vectors - Exercice 2

--- entities/File.java
+++ entities/File.java
@@ -6,26 +6,29 @@ public class File {
 
         Scanner sc = new Scanner(System.in);
         
-        int n = sc.nextInt();
-        int[] vect = new int [n];
-   
+        int number = sc.nextInt();
+        double[] vect = new double [number];
+        double sum = 0;
+        double average = 0.0;
 
         for(int i = 0; i < vect.length ; i++){
-            System.out.println("Digite um número.");
+            System.out.println("Type a number.");
             vect[i] = sc.nextInt();
+            sum += vect[i];
         }
 
-        System.out.println("Negative numbers : ");
+        System.out.println("Values : ");
 
         for(int i = 0; i < vect.length ; i++){
 
-            if (vect[i] < 0) {
-           
             System.out.println(vect[i]);
-            
-            }
+
             
         }
+
+        System.out.printf("Sum = %.2f\n", sum);
+        System.out.printf("Average : %.2f", average = sum / number);
+        
         sc.close();
     }
 }
