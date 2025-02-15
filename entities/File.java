3b4a0aaef7f3567b5960464c8598b0669ed16bdb
Emerson Lucas Bertelli Cardoso <emersonn.lbc@gmail.com>
2025-02-15 14:54:02 +0000

    Vectors - Exercice 3

--- entities/File.java
+++ entities/File.java
@@ -7,28 +7,48 @@ public class File {
         Scanner sc = new Scanner(System.in);
         
         int number = sc.nextInt();
-        double[] vect = new double [number];
-        double sum = 0;
-        double average = 0.0;
-
-        for(int i = 0; i < vect.length ; i++){
-            System.out.println("Type a number.");
-            vect[i] = sc.nextInt();
-            sum += vect[i];
-        }
-
-        System.out.println("Values : ");
+        int count = 0;
+        double percentual = 0.0;
+        double avg = 0.0;
+        double sum = 0.0;
+        
+        People[] vect = new People[number];
+        String[] vectName = new String[count];
+      
 
         for(int i = 0; i < vect.length ; i++){
 
-            System.out.println(vect[i]);
-
+            System.out.println("Enter the person's name, age, and height.");
+            sc.nextLine();
+            String name = sc.nextLine();
+            int age = sc.nextInt();
+            double height = sc.nextDouble();
+            vect[i] = new People(name, age, height);
+            sum += vect[i].height;
             
         }
+            
 
-        System.out.printf("Sum = %.2f\n", sum);
-        System.out.printf("Average : %.2f", average = sum / number);
+        for(int i = 0; i < vect.length ; i++){
+
+            if (vect[i].age < 16) {
+                
+                count ++;
+                
+            }
+            
+        }
         
+
+        System.out.printf("Average heigt: %.2f\n", avg = sum / number);
+        System.out.printf("Percentage of people under 16 years old: %.2f%%\n", percentual = (count * 100) / number);
+
+        for(int i = 0; i < vect.length ; i++){
+            if(vect[i].age < 16 ){
+                System.out.println(vect[i].name);
+            }
+        }
+
         sc.close();
     }
 }
