038b7f0692d7a1d25292626c453ec286c652600e
Emerson Lucas Bertelli Cardoso <emersonn.lbc@gmail.com>
2025-02-20 19:38:32 +0000

    List

--- entities/File.java
+++ entities/File.java
@@ -1,54 +1,38 @@
 package entities;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class File {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
-        int number = sc.nextInt();
-        int count = 0;
-        double percentual = 0.0;
-        double avg = 0.0;
-        double sum = 0.0;
-        
-        People[] vect = new People[number];
-        String[] vectName = new String[count];
-      
+    
+        List<String> list = new ArrayList<>();
 
-        for(int i = 0; i < vect.length ; i++){
+        list.add("Emerson");
+        list.add("Lucas");
+        list.add("Bertelli");
+        list.add("Cardoso");
 
-            System.out.println("Enter the person's name, age, and height.");
-            sc.nextLine();
-            String name = sc.nextLine();
-            int age = sc.nextInt();
-            double height = sc.nextDouble();
-            vect[i] = new People(name, age, height);
-            sum += vect[i].height;
-            
-        }
-            
-
-        for(int i = 0; i < vect.length ; i++){
-
-            if (vect[i].age < 16) {
-                
-                count ++;
-                
-            }
-            
-        }
-        
-
-        System.out.printf("Average heigt: %.2f\n", avg = sum / number);
-        System.out.printf("Percentage of people under 16 years old: %.2f%%\n", percentual = (count * 100) / number);
-
-        for(int i = 0; i < vect.length ; i++){
-            if(vect[i].age < 16 ){
-                System.out.println(vect[i].name);
-            }
+        list.remove(1);
+         
+        for (String obj : list) {
+            System.out.println(obj);
         }
 
-        sc.close();
+        System.out.println("-----------------------------------------");
+
+        list.removeIf(obj -> String.valueOf(obj).charAt(0) == '1');
+
+        for (String obj : list) {
+            System.out.println(obj);
+        }
+
+        System.out.println("-----------------------------------------");
+
+        String value = list.stream().filter(obj ->obj.charAt(0) == 'C').findFirst().orElse(null);
+        System.out.println(value);
     }
 }
