fcca46a140b65179f3ae26c80522c1c313b584c0
Emerson Lucas Bertelli Cardoso <emersonn.lbc@gmail.com>
2025-03-06 13:04:19 +0000

    DateTime, formating, Instant

--- main/Main.java
+++ main/Main.java
@@ -1,33 +1,30 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int count = 0;
-        int n = sc.nextInt();
-        int [][] matrix = new int [n][n];
+        
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
+        LocalDateTime d04 = LocalDateTime.now();
 
-        for(int i = 0 ; i < n; i++){
-            for(int j = 0 ; j < n; j++){
-                System.out.println("Enter the value for row " + (i + 1) + " and column " + (j + 1));
-                matrix[i][j] = sc.nextInt();
-            }
-        }
+        DateTimeFormatter formatin = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        
 
-        for(int i = 0 ; i < n; i++){
-            System.out.print(matrix[i][i] + " ");
-        }
-        System.out.println();
+        
+        Instant d05 = Instant.parse("2025-06-03T08:36:12Z");
 
-        for(int i = 0 ; i < n; i++){
-            for(int j = 0 ; j < n; j++){
-                if ((matrix[i][j] < 0)) {
-                    count++;
-                }
-            }
-        }
-        System.out.println("Negative numbers = " + count);
+        System.out.println(d01);
+        System.out.println(d02);
+        System.out.println(d03);
+        System.out.println(d04.format(formatin));
+        System.out.println(d05);
         sc.close();
     }
 }
