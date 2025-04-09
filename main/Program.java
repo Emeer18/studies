8126c1804ff4e66fcdfa120dc6688374e66bf8ef
Emerson Lucas Bertelli Cardoso <emersonn.lbc@gmail.com>
2025-04-09 14:41:28 +0000

    Excercice - polymorphism

--- main/Program.java
+++ main/Program.java
@@ -1,31 +1,60 @@
+import java.util.List;
 import java.util.Scanner;
-
-import Heritage.Account;
-import Heritage.BusinessAccount;
-import Heritage.SavingAccount;
-
+import Heritage.*;
+import java.util.ArrayList;
 import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) throws java.text.ParseException {
 
     Scanner sc = new Scanner(System.in);
+
+    List<Employee> employees = new ArrayList();
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     
-    Account acc = new Account(1001, "Alex", 500.0);
-    acc.withdraw(200.0);
-    System.out.println("Account: " + acc.getNumber() + ", Holder: " + acc.getHolder() + ", Balance: " + String.format("%.2f", acc.getBalance()));
-    System.out.println();
-    Account acc2 = new SavingAccount(1002, "Maria", 500.0);
-    acc2.withdraw(200.0);
-    System.out.println("Account: " + acc2.getNumber() + ", Holder: " + acc2.getHolder() + ", Balance: " + String.format("%.2f", acc2.getBalance()));
-    System.out.println();
+    System.out.println("Enter the number of employees: ");
+    int n = sc.nextInt();
+    sc.nextLine(); 
+    
+    for(int i = 0 ; i < n; i++){
 
-    Account acc3 = new BusinessAccount(1003, "Bob", 500.0, 400.0);
-    acc3.withdraw(200.0);
-    System.out.println("Account: " + acc3.getNumber() + ", Holder: " + acc3.getHolder() + ", Balance: " + String.format("%.2f", acc3.getBalance()));
-    System.out.println();  
+    System.out.println("Employee #" + (i+1) + " data: ");
+    System.out.println("Outsorce Employee? (y/n)");
+    char ch = sc.next().charAt(0);
+    sc.nextLine(); 
 
+            if (ch == 'y'){
 
+                System.out.println("Enter the name: ");
+                String name = sc.nextLine();
+                System.out.println("Enter the hours: ");
+                int hours = sc.nextInt();
+                System.out.println("Enter the value per hour: ");
+                double valuePerHour = sc.nextDouble();
+                System.out.println("Enter the additional charge: ");
+                double additionalCharge = sc.nextDouble();
+                sc.nextLine(); 
+            
+                employees.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+        
+            } else {
+
+                System.out.println("Enter the name: ");
+                String name = sc.nextLine();
+                System.out.println("Enter the hours: ");
+                int hours = sc.nextInt();
+                System.out.println("Enter the value per hour: ");
+                double valuePerHour = sc.nextDouble();
+                sc.nextLine(); 
+            
+                employees.add(new Employee(name, hours, valuePerHour));
+        }
+    }
+
+    System.out.println("PAYMENTS: ");
+    for (Employee emp : employees) {
+        System.out.println(emp.getName() + " - $ " + String.format("%.2f", emp.payment()));
+    }
 
     sc.close();
     }
