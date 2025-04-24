d6adaaeb3d8d6f37be9476f4ef8c5f20184cdf3b
Emerson Lucas Bertelli Cardoso <emersonn.lbc@gmail.com>
2025-04-24 14:38:48 +0000

    Inheritance and polymorphism exercise

--- main/Program.java
+++ main/Program.java
@@ -1,61 +1,74 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import Heritage.*;
-import java.util.ArrayList;
-import java.text.SimpleDateFormat;
+
+import Heritage.Product;
+import Heritage.UsedProduct;
+import Heritage.ImportedProduct;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Program {
     public static void main(String[] args) throws java.text.ParseException {
 
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter frmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        List<Product> products = new ArrayList<>();
 
-    List<Employee> employees = new ArrayList();
-    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-    
-    System.out.println("Enter the number of employees: ");
-    int n = sc.nextInt();
-    sc.nextLine(); 
-    
-    for(int i = 0 ; i < n; i++){
+        System.out.println("Enter the number of products: ");
+        int n = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
 
-    System.out.println("Employee #" + (i+1) + " data: ");
-    System.out.println("Outsorce Employee? (y/n)");
-    char ch = sc.next().charAt(0);
-    sc.nextLine(); 
+        for (int i = 0; i < n; i++) {
 
-            if (ch == 'y'){
+            System.out.println("Common, used or imported (c/u/i)? ");
+            char type = sc.nextLine().charAt(0);
 
-                System.out.println("Enter the name: ");
+            if (type == 'u') {
+                System.out.println("Enter product name: ");
                 String name = sc.nextLine();
-                System.out.println("Enter the hours: ");
-                int hours = sc.nextInt();
-                System.out.println("Enter the value per hour: ");
-                double valuePerHour = sc.nextDouble();
-                System.out.println("Enter the additional charge: ");
-                double additionalCharge = sc.nextDouble();
-                sc.nextLine(); 
-            
-                employees.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
-        
-            } else {
+                System.out.println("Enter product price: ");
+                double price = sc.nextDouble();
+                sc.nextLine(); // Consume the newline character
+                System.out.println("Enter manufacture date (DD/MM/YYYY): ");
+                String dateInput = sc.nextLine();
+                LocalDateTime manufactureDate = LocalDateTime.now();
 
-                System.out.println("Enter the name: ");
+                Product product = new UsedProduct(name, price, frmt.format(manufactureDate));
+                products.add(product);
+            }
+
+            if (type == 'i') {
+                System.out.println("Enter product name: ");
                 String name = sc.nextLine();
-                System.out.println("Enter the hours: ");
-                int hours = sc.nextInt();
-                System.out.println("Enter the value per hour: ");
-                double valuePerHour = sc.nextDouble();
-                sc.nextLine(); 
-            
-                employees.add(new Employee(name, hours, valuePerHour));
+                System.out.println("Enter product price: ");
+                double price = sc.nextDouble();
+                System.out.println("Enter customs fee: ");
+                double customsFee = sc.nextDouble();
+                sc.nextLine(); // Consume the newline character
+
+                Product product = new ImportedProduct(name, price, customsFee);
+                products.add(product);
+            }
+
+            if (type == 'c') {
+                System.out.println("Enter product name: ");
+                String name = sc.nextLine();
+                System.out.println("Enter product price: ");
+                double price = sc.nextDouble();
+                sc.nextLine(); // Consume the newline character
+
+                Product product = new Product(name, price);
+                products.add(product);
+            }
         }
-    }
 
-    System.out.println("PAYMENTS: ");
-    for (Employee emp : employees) {
-        System.out.println(emp.getName() + " - $ " + String.format("%.2f", emp.payment()));
-    }
+        for (Product product : products) {
+            System.out.println(product.priceTag());
+        }
 
-    sc.close();
+        sc.close();
     }
 }
